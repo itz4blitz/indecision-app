6cf7c55a1657f46d84c5ce5860fa1f1b11968ba5
Justin Scroggins <justinscroggins@outlook.com>
2022-06-20 19:02:12 -0400

    Extend a class

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,95 +1,75 @@
 'use strict';
 
-console.log('App.js is running!');
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
 
-// if statements
-// ternary operators
-// logical and operator
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var appObject = {
-    title: 'Indecision App',
-    subtitle: 'An app built using React',
-    options: []
-};
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-var onFormSubmit = function onFormSubmit(e) {
-    e.preventDefault();
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-    var option = e.target.elements.option.value;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-    if (option) {
-        appObject.options.push(option);
-        e.target.elements.option.value = '';
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Anonymous";
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+
+        _classCallCheck(this, Person);
+
+        this.name = name;
+        this.age = age;
     }
-    render();
-};
 
-var appRoot = document.getElementById('app');
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            return 'Hi! ' + this.name;
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name + ' is ' + this.age;
+        }
+    }]);
 
-var onRemoveAll = function onRemoveAll() {
-    appObject.options = [];
-    render();
-};
+    return Person;
+}();
 
-var onMakeDecision = function onMakeDecision() {
-    var randomNum = Math.floor(Math.random() * appObject.options.length);
-    var option = appObject.options[randomNum];
-    alert(option);
-};
+var Student = function (_Person) {
+    _inherits(Student, _Person);
 
-var render = function render() {
-    var template = React.createElement(
-        'div',
-        null,
-        appObject.title && React.createElement(
-            'h1',
-            null,
-            appObject.title
-        ),
-        appObject.subtitle && React.createElement(
-            'p',
-            null,
-            appObject.subtitle
-        ),
-        React.createElement(
-            'p',
-            null,
-            appObject.options.length > 0 ? 'Here are your options' : 'No options'
-        ),
-        React.createElement(
-            'button',
-            { disabled: appObject.options.length === 0, onClick: onMakeDecision },
-            'What should I do?'
-        ),
-        React.createElement(
-            'button',
-            { onClick: onRemoveAll },
-            'Remove All'
-        ),
-        React.createElement(
-            'ol',
-            null,
-            appObject.options.map(function (item) {
-                return React.createElement(
-                    'li',
-                    { key: item },
-                    item
-                );
-            })
-        ),
-        React.createElement(
-            'form',
-            { onSubmit: onFormSubmit },
-            React.createElement('input', { type: 'text', name: 'option' }),
-            React.createElement(
-                'button',
-                null,
-                'Add Option'
-            )
-        )
-    );
+    function Student(name, age, major) {
+        _classCallCheck(this, Student);
 
-    ReactDOM.render(template, appRoot);
-};
+        var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
 
-render();
+        _this.major = major;
+        return _this;
+    }
+
+    _createClass(Student, [{
+        key: 'hasMajor',
+        value: function hasMajor() {
+            return !!this.major;
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
+
+            if (this.hasMajor()) {
+                description += ' their major is ' + this.major;
+            }
+            return description;
+        }
+    }]);
+
+    return Student;
+}(Person);
+
+var me = new Student('Justin', 33, 'Computer Science');
+console.log(me.getDescription());
+
+var other = new Student();
+console.log(other.getDescription());
---
+++ src/playground/es6-classes-1.js
@@ -0,0 +1,36 @@
+class Person {
+    constructor(name = "Anonymous", age = 0){
+       this.name = name; 
+       this.age = age;
+    }
+    getGreeting() {
+        return `Hi! ${this.name}`
+    }
+    getDescription() {
+        return `${this.name} is ${this.age}`
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, major){
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += ` their major is ${this.major}`
+        }
+        return description;
+    }
+}
+
+const me = new Student('Justin', 33, 'Computer Science');
+console.log(me.getDescription());
+
+const other = new Student();
+console.log(other.getDescription());
